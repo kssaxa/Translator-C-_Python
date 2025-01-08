b580445e7789313deb9d0d58fe2af3699dc79d01
Ksenia Egorova <118151597+Muwert@users.noreply.github.com>
2025-01-08 12:36:43 +1000

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -51,7 +51,9 @@ class BlockNode(ExpressionNode):
     def __init__(self, keyword: Token):
         super().__init__()
         self.keyword = keyword
+        self.condition = None  # поле для условия
         self.body = []
+        self.else_branch = None  # Ветка else или else if
 
     def add_node(self, node: ExpressionNode):
         self.body.append(node)
@@ -67,3 +69,56 @@ class ElseNode(ExpressionNode):
 
     def add_node(self, node: ExpressionNode):
         self.body.append(node)
+
+
+class ElseIfNode(ExpressionNode):
+    """Узел для блока else if."""
+
+    def __init__(self, keyword: Token, condition: ExpressionNode):
+        super().__init__()
+        self.keyword = keyword
+        self.condition = condition
+        self.body = []
+
+    def add_node(self, node: ExpressionNode):
+        self.body.append(node)
+
+
+class FunctionNode(ExpressionNode):
+    """Узел для функции."""
+    
+    def __init__(self, return_type: Token, name_token: str, parameters: list, body: ExpressionNode):
+        super().__init__()
+        self.return_type = return_type 
+        self.name_token = name_token
+        self.parameters = parameters  
+        self.body = body  
+
+class ReturnNode(ExpressionNode):
+    """Узел для ключевого слова return."""
+    
+    def __init__(self, keyword: Token):
+        super().__init__()
+        self.keyword = keyword
+
+
+class VariableDeclarationNode(ExpressionNode):
+    """Узел для объявления переменной."""
+    
+    def __init__(self, var_type: Token, var_name: Token, value: ExpressionNode = None):
+        super().__init__()
+        self.var_type = var_type  
+        self.var_name = var_name  
+        self.value = value  
+
+
+class FuncNode:
+    """Узел для функций ввода/вывода."""
+
+    def __init__(self, func_token, arguments):
+        self.func_token = func_token  # Токен функции (например, cin)
+        self.arguments = arguments  # Список аргументов функции
+        self.operator = None
+
+    def add_argument(self, argument):
+        self.arguments.append(argument)
